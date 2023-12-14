03c99358b893ef6a26cb4bdce93dc8024f6a61c8
vsavvina <82946852+vsavvina@users.noreply.github.com>
2023-12-14 17:03:51 +0300

    new search

---
+++ src/test/java/Searchtests.java
@@ -0,0 +1,24 @@
+import org.bouncycastle.operator.bc.BcSignerOutputStream;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+public class Searchtests {
+  @BeforeAll
+  static void speedOnPage() {
+    Configuration.pageLoadStrategy = "eager";
+  }
+  @Test
+  void selenideSearchTest() {
+            open("https://www.google.ru");
+            $("[name=q]").setValue("Selenide").pressEnter();
+            $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+  }
+  @AfterAll
+  static void successfulStatus () {
+    System.out.println("\nTest pass happy\n");
+  }
+}

2850367f5073898670f7a8740af6767cdc1770c2
Виктория Олеговна Саввина <82946852+vsavvina@users.noreply.github.com>
2023-12-19 13:22:44 +0300

    Update Searchtests.java

--- src/test/java/Searchtests.java
+++ src/test/java/Searchtests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class Searchtests {
   @BeforeAll
   static void speedOnPage() {
-    Configuration.pageLoadStrategy = "eager";
+    Configuration.pageLoadStrategy = "eager";//uskorenie raboti stranici
   }
   @Test
   void selenideSearchTest() {

0c5009252c37a46ad4257e8f736110c0cec5d702
vsavvina <82946852+vsavvina@users.noreply.github.com>
2023-12-19 12:06:50 +0300

    добавление комментария

--- src/test/java/Searchtests.java
+++ src/test/java/Searchtests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class Searchtests {
   @BeforeAll
   static void speedOnPage() {
-    Configuration.pageLoadStrategy = "eager";
+    Configuration.pageLoadStrategy = "eager";  //ускорение загрузки страницы
   }
   @Test
   void selenideSearchTest() {
